35e922bb1d2d079975cc30ea72e99edff55c3471
fuwx <f2928560492@163.com>
2024-05-12 22:33:13 +0800

    fix args

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,10 +1,9 @@
-use clap::{Args, Parser, Subcommand};
+use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-#[command(propagate_version = true)]
 pub struct Cli {
-    /// queryformat base/info/changelog/require
+    /// queryformat base/info/changelog/require/provid e
     #[arg(short, long)]
     pub query: char,
 
@@ -12,24 +11,6 @@ pub struct Cli {
     #[arg(short, long)]
     pub all: bool,
 
-    /// query package by name or local
-    #[command(subcommand)]
-    pub command: Option<Commands>,
-}
-#[derive(Subcommand)]
-pub enum Commands {
-    /// package name
-    Name(PackageName),
-    /// package filepath
-    File(FileName),
-}
-
-#[derive(Args, Clone)]
-pub struct PackageName {
-    pub name: Vec<String>,
-}
-
-#[derive(Args, Clone)]
-pub struct FileName {
-    pub files: Vec<String>,
+    /// package name or file
+    pub name: Option<Vec<String>>,
 }
--- src/query/mode.rs
+++ src/query/mode.rs
@@ -1,5 +1,5 @@
 use crate::{
-    cli::{Cli, Commands},
+    cli::Cli,
     rpm::db::{find_package, installed_packages},
 };
 
@@ -13,44 +13,48 @@ impl QueryMode {
         let mut keys: Vec<String> = Vec::new();
         match cli.all {
             true => pkgs = installed_packages(cli.query).collect(),
-            false => match &cli.command {
-                Some(cmd) => match cmd {
-                    Commands::Name(name) => {
-                        keys.extend(name.name.clone());
-                        for key in keys.iter() {
-                            pkgs = find_package(key.clone(), cli.query).collect();
-                        }
-                    }
-                    Commands::File(file) => {
-                        for f in file.files.iter() {
-                            let p = rpm::Package::open(f).unwrap();
-                            match cli.query {
-                                'b' => {
-
-                                    println!("{}-{}-{}.{}", p.metadata.get_name().unwrap(), p.metadata.get_version().unwrap(), p.metadata.get_release().unwrap(), p.metadata.get_arch().unwrap());
-                                },
-                                'i' => {},
-                                'c' => {
-                                    for cl in p.metadata.get_changelog_entries().unwrap() {
-                                        println!("{}\n{}\n", cl.name, cl.description);
+            false => match &cli.name {
+                Some(names) => {
+                    for name in names.iter() {
+                        match name.ends_with(".rpm") {
+                            true => {
+                                
+                                    let p = rpm::Package::open(name).unwrap();
+                                    match cli.query {
+                                        'b' => {
+        
+                                            println!("{}-{}-{}.{}", p.metadata.get_name().unwrap(), p.metadata.get_version().unwrap(), p.metadata.get_release().unwrap(), p.metadata.get_arch().unwrap());
+                                        },
+                                        'i' => {},
+                                        'c' => {
+                                            for cl in p.metadata.get_changelog_entries().unwrap() {
+                                                println!("{}\n{}\n", cl.name, cl.description);
+                                            }
+                                        },
+                                        'r' => {
+                                            for r in p.metadata.get_requires().unwrap() {
+                                                println!("{}", r.name);
+                                            }
+                                        },
+                                        'p' => {
+                                            for pv in p.metadata.get_provides().unwrap() {
+                                                println!("{}", pv.name);
+                                            }
+                                        }
+                                        _ => (),
                                     }
-                                },
-                                'r' => {
-                                    for r in p.metadata.get_requires().unwrap() {
-                                        println!("{}", r.name);
-                                    }
-                                },
-                                'p' => {
-                                    for pv in p.metadata.get_provides().unwrap() {
-                                        println!("{}", pv.name);
-                                    }
+                                
+        
+                            return;
+                            }
+                            false => {
+                                keys.push(name.clone());
+                                for key in keys.iter() {
+                                    pkgs = find_package(key, cli.query).collect();
                                 }
-                                _ => (),
                             }
                         }
-
-                    return;
-                    },
+                    }
                 },
                 None => (),
             },
